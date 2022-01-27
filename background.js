b2bb3daf1e5a0ee8d71642be7389f6755cb10376
Jackie <taekwondo.chan@gmail.com>
2022-01-26 17:38:52 -0800

    create background class

---
+++ background.js
@@ -0,0 +1,9 @@
+import Sprite from './sprite';
+
+class Background extends Sprite {
+  constructor(color) {
+    super(undefined, undefined, undefined, undefined, color);
+  }
+}
+
+export default Background;
