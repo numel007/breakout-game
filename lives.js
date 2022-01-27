5f8c2c116ee44f8a8280aaaf995e518a421d5b0f
Jackie <taekwondo.chan@gmail.com>
2022-01-26 17:39:37 -0800

    add lives class

---
+++ lives.js
@@ -0,0 +1,27 @@
+import Sprite from './sprite.js';
+
+class Lives extends Sprite {
+  constructor(width, color, lives = 3, font = '16px Arial') {
+    super();
+    this.width = width;
+    this.lives = lives;
+    this.font = font;
+    this.color = color;
+  }
+
+  loseLife() {
+    this.lives -= 1;
+  }
+
+  reset() {
+    this.lives = 3;
+  }
+
+  render(ctx) {
+    ctx.font = this.font;
+    ctx.fillStyle = this.color;
+    ctx.fillText(`Lives: ${this.lives}`, this.width - 65, 20);
+  }
+}
+
+export default Lives;
