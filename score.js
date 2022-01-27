c0cecdc6f46f2c7b0d6d268ebd7a4be94dbf11f2
Jackie <taekwondo.chan@gmail.com>
2022-01-26 17:39:03 -0800

    add score class

---
+++ score.js
@@ -0,0 +1,26 @@
+import Sprite from './sprite.js';
+
+class Score extends Sprite {
+  constructor(x, y, color, score = 0, font = '16px Arial') {
+    super();
+    this.score = score;
+    this.font = font;
+    this.color = color;
+  }
+
+  update(points) {
+    this.score += points;
+  }
+
+  reset() {
+    this.score = 0;
+  }
+
+  render(ctx) {
+    ctx.font = this.font;
+    ctx.fillStyle = this.color;
+    ctx.fillText(`Score: ${this.score}`, 8, 20);
+  }
+}
+
+export default Score;
